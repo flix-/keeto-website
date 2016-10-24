79f21cfbda624a9f6541743348b245518645fb78
Björn Roland <bjoern.roland@gmail.com>
2016-10-25 00:45:23 +0200

    ongoing refactoring

--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+	$('.header-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		accessibility: true,
+		arrows: true,
+	});
 });
